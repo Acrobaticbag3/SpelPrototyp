0ec71dbeb96145e2829ba52f5c05a848ac946555
Acrobaticbag3 <kevintom214@gmail.com>
2023-03-07 14:13:13 +0100

    h

--- SpelPrototyp/Assets/Scripts/PlayerScripts/Movement.cs
+++ SpelPrototyp/Assets/Scripts/PlayerScripts/Movement.cs
@@ -1,22 +1,21 @@
-/*
-    This Script was written by Kevin Johansson.
-*/
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Movement : MonoBehaviour
-{
+public class Movement : MonoBehaviour {
     private Rigidbody rb;
-    public Transform transform;
+    public Transform target;
     public Camera camera;
 
     // Ground movement \\
-    public float speed = 200f;
-    public float sprintSpeed = 300f;
-    private float stamina = 100;
+    private float speed = 200f;
+    private float sprintSpeed = 400f;
 
+    // Stammina and stammina regen \\
+    private float currentStamina = 100;
+    [SerializeField] private RawImage staminaBar;
+    
     // Jumping and gravity \\
     public float jumpHeight = 1;
     public float gravityScale = 10;
@@ -29,31 +28,29 @@ public class Movement : MonoBehaviour
     // Colider \\
     public CapsuleCollider col;
 
-    void Start()
-    {
-        camera.fieldOfView = 80;
+    
+
+    void Start() {
         rb = GetComponent<Rigidbody>();
+        camera.fieldOfView = 80;
     }
 
-    void Update()
-    {   // Everything after this code stops when paused 
+    void Update() {   
+        // Everything after this code stops when paused 
         // if code wants to be run even if pasued put before
-        if (GameManager.isPaused || GameManager.swtichingSpells)
-        {
+        if (GameManager.isPaused || GameManager.swtichingSpells) {
             return;
         } 
         
         //Jumping
-        if (Input.GetKeyDown(KeyCode.Space) && rb.velocity.y == 0 && stamina != 0)
-        {
+        if (Input.GetKeyDown(KeyCode.Space) && rb.velocity.y == 0 && currentStamina != 0) {
             rb.AddForce(Vector3.up * jumpHeight, ForceMode.Impulse);
-            stamina = stamina - 20;
+            currentStamina = currentStamina - 20;
         }
-
     }
 
-    void FixedUpdate()
-    {
+    void FixedUpdate() {
+
         float verticalPlayerInput = Input.GetAxisRaw(axisName: "Vertical");             // Gets vertical input.
         float horizontalPlayerInput = Input.GetAxisRaw(axisName: "Horizontal");         // Gets horizontal input.
 
@@ -67,32 +64,29 @@ public class Movement : MonoBehaviour
         right = right.normalized;
 
         float speed = this.speed;
-        Vector3 forwardRelativeVerticalInput = verticalPlayerInput * forward * Time.fixedDeltaTime;         // Fixes relative movment for vertical movment.
-        Vector3 rightRelativeHorizontalInput = horizontalPlayerInput * right * Time.fixedDeltaTime;         // Fixes relative movment for horizontal movment.
+        Vector3 forwardRelativeVerticalInput = verticalPlayerInput * -transform.forward * Time.fixedDeltaTime;         // Fixes relative movment for vertical movment. Note -transform is a temp fix
+        Vector3 rightRelativeHorizontalInput = horizontalPlayerInput * -transform.right * Time.fixedDeltaTime;         // Fixes relative movment for horizontal movment. Note -transform is a temp fix
 
         Vector3 cameraRelativeMovement = forwardRelativeVerticalInput + rightRelativeHorizontalInput;
+        Vector3 targetPosition = new Vector3(target.transform.position.x, transform.position.y, target.transform.position.z); 
+        transform.LookAt(targetPosition);
 
-        if (Input.GetKey(key: KeyCode.LeftShift) && stamina != 0)
-        {
+        if (Input.GetKey(key: KeyCode.LeftShift) && currentStamina != 0) {
             speed = sprintSpeed;
             camera.fieldOfView = 90;
-            stamina = stamina - 3;
-
+            currentStamina = currentStamina - 3;
         }
-        else
-        {
-            stamina++;
+        else {
+            currentStamina++;
             camera.fieldOfView = 80;
         }
 
         transform.Translate(translation: cameraRelativeMovement * speed * Time.deltaTime, relativeTo: Space.World);
 
-        if (Input.GetButton("Crouch"))
-        {
+        if (Input.GetButton("Crouch")) {
             col.height = Mathf.Max(0.6f, col.height - Time.deltaTime * 10.0f);
         }
-        else
-        {
+        else {
             col.height = Mathf.Min(1.8f, col.height + Time.deltaTime * 10.0f);
         }
     }
